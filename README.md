639845e20f765efafebfc100dbda2407ed7f36c3
Letourneau <n0117008@libp45p-34855wl.mis.lmig.com>
2017-11-22 10:26:15 -0500

    Added in soem comments to make it easier to read

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Service2Compose -stack=<string>
 will go through find all stacks matching string and attempt to rebuild compose file for that stack 
 
 If not -stack specified then does all stacks it finds.
+
+Assumes you are running inside a client bundle.
--- Service2Compose.go
+++ Service2Compose.go
@@ -13,31 +13,41 @@ import (
 	"strings"
 	"strconv"
 )
-
+//  Program assumes you are running inside an environment setup with a client bundle
 
 func main() {
+	//setup flags, right now just one	
+	stackPtr := flag.String("stack", "*", "a string of the pattern to match for stacks")
+	flag.Parse()
+
+	//setup client environment
+
 	cli, err := client.NewEnvClient()
-	//theServices := make(map[string][]map[string]string)
 	if err != nil {
 		panic(err)
 	}
 
+	//grab all the services
     	services, err := cli.ServiceList(context.Background(),types.ServiceListOptions{})
 	if err != nil {
 		panic(err)
 	}
+	//grab all the networks
     	networks, err := cli.NetworkList(
 		context.Background(),
 		types.NetworkListOptions{})
 	if err != nil {
 		panic(err)
 	}
+	//call me lazy but I want to be able to refer easily to a network by its ID so create a mapping
+	//between the id and the actual network resource
 	theNetworks := make(map[string]types.NetworkResource)
 	for _, network := range networks {
 		theNetworks[network.ID] = network
 	}
-	stackPtr := flag.String("stack", "*", "a string")
-	flag.Parse()
+	//Grab all the stacks, unfortunately stacks is really represented as a label on a service
+	//so we need to go through all the services and and get all the values for that label AND
+	//while we are at it we should keep track of all the services for that stack
 	stacks := make(map[string][]string)
 	theServices := make(map[string]swarm.Service)
 	for _, service := range services {
@@ -50,6 +60,7 @@ func main() {
 		}
 		theServices[service.ID] = service
 	}
+	//setup stuff to do matching for the stackname and what we actually want for output
 	matched := false
 	all := false
 	wildcard := strings.Compare(*stackPtr,"*")
@@ -60,23 +71,31 @@ func main() {
     }    
 	fmt.Println("Request is -",*stackPtr)
 	fmt.Println()
+	//so now we go through the stacks to find the one(s) that match the request and then do some work
 	for stackname, services := range stacks {
 		matched, _ = regexp.MatchString(*stackPtr,stackname)
 		if matched || all {
 			fmt.Println("Stackname is - ", stackname)
+			fmt.Println()
+			//And here we go actually dumping out the compose
 			fmt.Printf("version: '3'\n\n")
 			fmt.Println("services:")
 			myNetworks := make(map[string]string)
 			for _,serviceID := range services {
+				//So maybe just particular to how we are doing composes, but we are using the alias and
+				//we aren't using multiple aliases so really I am JUST setting up the initial one
+				//unless of course somehow thats not been set, then I just put the service name in there
 				if len(theServices[serviceID].Spec.Networks) != 0 && len(theServices[serviceID].Spec.Networks[0].Aliases) != 0 {
 					fmt.Printf("  %s:\n",theServices[serviceID].Spec.Networks[0].Aliases[0])
 				} else {
 					fmt.Printf("  %s:\n", theServices[serviceID].Spec.Name)
 				}	
+				//You have to have an image, I mean really and you at least need one replica
 				fmt.Println("    image: ",theServices[serviceID].Spec.TaskTemplate.ContainerSpec.Image)
 				fmt.Println("    deploy:")
 				replicas := uint64(*theServices[serviceID].Spec.Mode.Replicated.Replicas)
 				fmt.Println("      replicas: ",strconv.FormatUint(replicas,10))
+				//if they have a restart policy, we need to deal with it
 				if theServices[serviceID].Spec.TaskTemplate.RestartPolicy != nil {
 					fmt.Println("      restart_policy:")
 					fmt.Println("        condition: ",theServices[serviceID].Spec.TaskTemplate.RestartPolicy.Condition)
@@ -84,6 +103,7 @@ func main() {
 					fmt.Println("        max_attempts: ",strconv.FormatUint(uint64(*theServices[serviceID].Spec.TaskTemplate.RestartPolicy.MaxAttempts),10))
 					fmt.Println("        window: ",theServices[serviceID].Spec.TaskTemplate.RestartPolicy.Window)
 				}
+				//if they have constraints we need to deal with that
 				if len(theServices[serviceID].Spec.TaskTemplate.Placement.Constraints) != 0 {
 					fmt.Println("      placement: ")
 					fmt.Println("        constraints:")
@@ -96,6 +116,7 @@ func main() {
 						}	
 					}
 				}
+				//Ahhh yes the multiple locations of labels, this is for the deploy section
 				if len(theServices[serviceID].Spec.Annotations.Labels) != 0 {
 					fmt.Println("      labels:")
 					for key, value := range theServices[serviceID].Spec.Annotations.Labels {
@@ -103,6 +124,7 @@ func main() {
 						fmt.Printf("%s=%s\n",strings.Trim(key," "),strings.Trim(value," "))
 					}
 				}
+				//And any published ports...
 				if len(theServices[serviceID].Endpoint.Spec.Ports) != 0  {
 					fmt.Println("    ports:")
 					for _, port := range theServices[serviceID].Endpoint.Spec.Ports {
@@ -110,6 +132,7 @@ func main() {
 						fmt.Printf("\"%d:%d\"\n",port.PublishedPort,port.TargetPort)
 					}	
 				}
+				//yes you can have a service on no network, so we need to check that
 				if len(theServices[serviceID].Spec.Networks) != 0 {
 					fmt.Println("    networks:")
 					for _, thisNetwork := range theServices[serviceID].Spec.Networks {
@@ -117,12 +140,14 @@ func main() {
 						myNetworks[thisNetwork.Target] = theNetworks[thisNetwork.Target].Name
 					}
 				}
+				//labels again, for the service specification
 				if len(theServices[serviceID].Spec.TaskTemplate.ContainerSpec.Labels) != 0 {
 					fmt.Println("    labels:")
 					for key, value := range theServices[serviceID].Spec.TaskTemplate.ContainerSpec.Labels {
 						fmt.Printf("      - %s=%s\n",key,value)
 					}
 				}	
+				//and any environment variables
 				if len(theServices[serviceID].Spec.TaskTemplate.ContainerSpec.Env) > 0 {
 					fmt.Println("    environment:")
 					for _, envVar := range theServices[serviceID].Spec.TaskTemplate.ContainerSpec.Env {
@@ -130,6 +155,7 @@ func main() {
 					}
 					
 				}
+				//Log driver information gathered and provided
 				if theServices[serviceID].Spec.TaskTemplate.LogDriver != nil {
 					if theServices[serviceID].Spec.TaskTemplate.LogDriver.Name == "" &&
 					   len(theServices[serviceID].Spec.TaskTemplate.LogDriver.Options) != 0   {
@@ -153,6 +179,8 @@ func main() {
 				}
 				fmt.Println()	
 			}
+			//So networks, need to dump those out, I am assuming in our implementation that if its not
+			//external its overlay, and thats cause thats how we do it, call me lazy it works for us
 			if len(myNetworks) != 0 {
 				fmt.Println("networks:")
 				for netID,netName := range myNetworks {
